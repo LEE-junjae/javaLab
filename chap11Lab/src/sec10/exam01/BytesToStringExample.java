bc280dd3d9ef11f4eca91fba9d63757964896dd7
LEE-junjae <junjae486@naver.com>
2021-03-30 17:57:36 +0900

    chap11 test

--- chap11Lab/src/sec10/exam01/BytesToStringExample.java
+++ chap11Lab/src/sec10/exam01/BytesToStringExample.java
@@ -6,6 +6,7 @@ public class BytesToStringExample {
 		byte[] bytes = {73, 32, 108, 111, 118, 101, 32, 121, 111, 117};
 		String str = new String(bytes);
 		System.out.println(str);
+		System.out.println();
 		    }
 		}
 
