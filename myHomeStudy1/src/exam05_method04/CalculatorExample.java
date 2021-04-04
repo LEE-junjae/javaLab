6a3c8e0d5b9c073492b3513225bfbcaaa8fa9f73
LEE-junjae <junjae486@naver.com>
2021-04-04 17:50:20 +0900

    myHomeStudy1  예제5

--- myHomeStudy1/src/exam05_method04/CalculatorExample.java
+++ myHomeStudy1/src/exam05_method04/CalculatorExample.java
@@ -3,8 +3,9 @@ package exam05_method04;
 public class CalculatorExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Calculator myCalc = new Calculator();
+		myCalc.excute();
 	}
 
 }
---
+++ myHomeStudy1/src/exam05_method06/Calculator.java
@@ -0,0 +1,19 @@
+package exam05_method06;
+
+public class Calculator {
+	//Field
+	
+	
+	//Constructor
+	
+	
+	//Method
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+
+}
