193b3e45d34e6b4efd4ffcb94d1fd62e01ee36a0
LEE-junjae <junjae486@naver.com>
2021-04-04 23:00:17 +0900

    myHomeStudy1 예제6

---
+++ myHomeStudy1/src/exam06_instance01/CarExample.java
@@ -0,0 +1,13 @@
+package exam06_instance01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+
+		Car myCar = new Car("Æ÷¸£½¦");
+		Car yourCar = new Car("º¥Ã÷");
+		myCar.run();
+		yourCar.run();
+	}
+
+}
---
+++ myHomeStudy1/src/exam06_instance02/Calculator.java
@@ -0,0 +1,16 @@
+package exam06_instance02;
+
+public class Calculator {
+	//Field
+	static double pi = 3.14159; //앞에 static으로 시작하면 정적멤버로 사용됨
+	
+	//Method
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	
+	static int minus(int x, int y) {
+		return x - y;
+	}
+
+}
